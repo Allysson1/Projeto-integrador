f482e888de4b0bb0a6f2abe6d5503b096bfd2958
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2021-10-27 22:12:13 -0300

    teste-arquivos-php-no-servidor

---
+++ header.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Portal COVID</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/estilo.css">
+    <script src="/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<header class="menu">
+
+<img src="/images/imagem1.jpg" alt="">
+
+<nav>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#" class="nav-item dropdown">Covid-19</a></li>
+<li><a href="#">Contatos</a></li>
+<li><a href="#">Dados Atualizados</a></li>
+<li><a href="#">Quem Somos</a></li>
+</ul>
+
+</nav>
+
+</header>
+
+
+</body>
+
+</html>
