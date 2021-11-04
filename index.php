c585bf29a80d01602d629cd57db77c1e26f2f293
JulioG00 <jukagabriel00@gmail.com>
2021-11-03 22:31:37 -0300

    modificações na home index e css

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <head>
   <title>Portal Covid</title>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="../Projeto-integrador/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../Projeto-integrador/css/estilo.css">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../Projeto-integrador/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../Projeto-integrador/css/estilo.css">
 </head>
 
 <body>
@@ -17,8 +17,9 @@
     </header>
     
 
-    <div class="row">
+    
       <main>
+      <div class="row">
         <article class="col-sm-8 col-md-7 col-lg-8">
           <section>
 
@@ -35,7 +36,6 @@
           <section>
 
             <h1>O que você procura ?</h1>
-            <h3>Ultimas Notícias: </h3>
             <p>01/01/2021</p>
             <img src="#" alt="">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -48,7 +48,6 @@
           <section>
 
             <h1>O que você procura ?</h1>
-            <h3>Ultimas Notícias: </h3>
             <p>01/01/2021</p>
             <img src="#" alt="">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -61,7 +60,6 @@
           <section>
 
             <h1>O que você procura ?</h1>
-            <h3>Ultimas Notícias: </h3>
             <p>01/01/2021</p>
             <img src="#" alt="">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -74,13 +72,21 @@
         </article>
 
         <aside class="col-sm-4 col-md-5 col-lg-4">
+            
+            <section class="secredes">
             <h3>Redes Sociais:</h3>
-            <span class="fab fa-facebook-f"></span>
+              <img src="../Projeto-integrador/images/facebook.svg" alt="" class="redes">
+              <img src="../Projeto-integrador/images/instagram.svg" alt="" class="redes">
+              <img src="../Projeto-integrador/images/twitter.svg" alt="" class="redes">
+              <img src="../Projeto-integrador/images/linkedin.svg" alt="" class="redes">
+            </section>
+            
+            
         </aside>
-
+        </div>
       </main>
 
-    </div>
+    
 
   </div>
   <script src="../Projeto-integrador/js/bootstrap.bundle.min.js"></script>
