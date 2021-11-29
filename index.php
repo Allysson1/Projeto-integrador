b37ab8e5043034eff3c537032581a4f5b536e70c
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2021-11-29 19:25:05 -0300

    continuando a home

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="../Projeto-integrador/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="../Projeto-integrador/css/estilo.css">
 
@@ -21,58 +21,62 @@
     <main>
       <img src="../Projeto-integrador/images/index.png" class="img-fluid" id="img-header" alt="">
       <div class="row">
-        <article class="col-sm-8 col-md-7 col-lg-8">
-          <section class="secnews"> 
+        <article class="col-12 col-sm-12 col-md-7 col-lg-8 col-xl-8">
 
-            <h1 class="h1index">O que você procura ?</h1>
-            <h3 class="h3last">Ultimas Notícias: </h3>
-            <p>01/01/2021</p>
-            <img src="#" alt="">
-            <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
-              cumque enim magnam velit inventore harum dolores? Sapiente.</p>
-            <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
 
-          </section>
-          <section class="secnews">
+          <h1 class="h1index">O que você procura ?</h1>
+          <h3 class="h3last">Ultimas Notícias: </h3>
+          <p>01/01/2021</p>
 
-            <h3 class="h1news">O que você procura ?</h1>
-            <p>01/01/2021</p>
-            <img src="#" alt="">
-            <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
-              cumque enim magnam velit inventore harum dolores? Sapiente.</p>
-            <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
 
+          <section class="secnews float-md-start float-lg-start float-xl-start">
+            <img src="../projeto-integrador/images/sintoma1.png" alt="" class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4">
+            <p class="pindex col-xl-4">Veja agora os principais sintomas do Covid-19, o que fazer quando
+              sentir algum deles, e como se previnir para que o não transmita o vírus para ninguém próximo de você como
+              a família ou pessoas e amigos na rua.</p>
+            <a href="sintomas.php" class="btn btn-md btnindex ">Leia Mais...</a>
           </section>
 
-          <section class="secnews">
+
+          <section class="secnews float-md-end float-lg-end float-xl-end">
 
             <h3 class="h1news">O que você procura ?</h1>
-            <p>01/01/2021</p>
-            <img src="#" alt="">
-            <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
-              cumque enim magnam velit inventore harum dolores? Sapiente.</p>
-            <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
+              <p>01/01/2021</p>
+              <img src="#" alt="">
+              <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
+                cumque enim magnam velit inventore harum dolores? Sapiente.</p>
+              <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
 
           </section>
 
           <section class="secnews">
 
             <h3 class="h1news">O que você procura ?</h1>
-            <p>01/01/2021</p>
-            <img src="#" alt="">
-            <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
-              cumque enim magnam velit inventore harum dolores? Sapiente.</p>
-            <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
+              <p>01/01/2021</p>
+              <img src="#" alt="">
+              <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
+                cumque enim magnam velit inventore harum dolores? Sapiente.</p>
+              <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
+
+          </section>
+
+          <section class="secnews">
+
+            <h3 class="h1news">O que você procura ?</h1>
+              <p>01/01/2021</p>
+              <img src="#" alt="">
+              <p class="pindex">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Optio et cum rem. Quas accusantium, soluta iste modi minima quasi ipsum delectus ipsam
+                cumque enim magnam velit inventore harum dolores? Sapiente.</p>
+              <a href="#" class="btn btn-md btnindex">Leia Mais...</a>
 
           </section>
 
         </article>
 
-        <aside class="col-sm-4 col-md-5 col-lg-4">
+        <aside class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4">
 
           <section class="secredes">
             <h3 class="h3redes">Redes Sociais:</h3>
