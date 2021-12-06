19dbad33b655eeb352dc301da784fbc7afa7b206
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2021-12-06 14:21:54 -0300

    corrigindo conflitos

--- sintomas.php
+++ sintomas.php
@@ -281,7 +281,6 @@
         <footer class="footer">
             <?php include 'footer.php'; ?>
         </footer>
-        <p class="copy"><small>&copy; Copyright - Todos os direitos reservados - Ursinhos Felizes</small></p>
 
     </div>
 
